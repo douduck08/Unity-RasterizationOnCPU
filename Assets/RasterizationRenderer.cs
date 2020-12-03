f7d214767ac4f34a3105fa77395998670675c8a2
douduck08 <lu10082000@gmail.com>
2020-12-04 00:29:21 +0800

    fix depth

--- Assets/RasterizationRenderer.cs
+++ Assets/RasterizationRenderer.cs
@@ -45,7 +45,7 @@ public class PixelBuffer {
     public List<int> indexes = new List<int> ();
 
     // Varyings
-    public List<Vector4> positions = new List<Vector4> (); // positions in NDC
+    public List<Vector3> positions = new List<Vector3> (); // positions in NDC
 }
 
 public class RasterizationRenderer : MonoBehaviour {
@@ -96,7 +96,7 @@ public class RasterizationRenderer : MonoBehaviour {
 
         Color[] colorBuffer = result.GetPixels ();
         float[] depthBuffer = new float[colorBuffer.Length];
-        Clear (colorBuffer, depthBuffer, backgroundColor);
+        Clear (colorBuffer, depthBuffer, backgroundColor, 0f);
 
         foreach (var model in models) {
             if (!model.IsActive ()) {
@@ -118,7 +118,7 @@ public class RasterizationRenderer : MonoBehaviour {
         depthMap.Apply ();
     }
 
-    void Clear (Color[] colorBuffer, float[] depthBuffer, Color color, float depth = 1f) {
+    void Clear (Color[] colorBuffer, float[] depthBuffer, Color color, float depth) {
         var size = colorBuffer.Length;
         for (int i = 0; i < size; i++) {
             colorBuffer[i] = color;
@@ -137,9 +137,9 @@ public class RasterizationRenderer : MonoBehaviour {
         var pixelSize = new Vector2 (1f / resolution.x, 1f / resolution.y);
         var size = vertexBuffer.vertices.Count;
         for (int i = 0; i < size; i += 3) {
-            var v0 = new Vector2 (vertexBuffer.positions[i].x / vertexBuffer.positions[i].w, vertexBuffer.positions[i].y / vertexBuffer.positions[i].w);
-            var v1 = new Vector2 (vertexBuffer.positions[i + 1].x / vertexBuffer.positions[i + 1].w, vertexBuffer.positions[i + 1].y / vertexBuffer.positions[i + 1].w);
-            var v2 = new Vector2 (vertexBuffer.positions[i + 2].x / vertexBuffer.positions[i + 2].w, vertexBuffer.positions[i + 2].y / vertexBuffer.positions[i + 2].w);
+            var v0 = new Vector2 (vertexBuffer.positions[i].x, vertexBuffer.positions[i].y) / vertexBuffer.positions[i].w;
+            var v1 = new Vector2 (vertexBuffer.positions[i + 1].x, vertexBuffer.positions[i + 1].y) / vertexBuffer.positions[i + 1].w;
+            var v2 = new Vector2 (vertexBuffer.positions[i + 2].x, vertexBuffer.positions[i + 2].y) / vertexBuffer.positions[i + 2].w;
             var area = EdgeFunction (v0, v1, v2);
 
             var xMin = Mathf.Max (Mathf.FloorToInt ((Mathf.Min (v0.x, v1.x, v2.x) * 0.5f + 0.5f) / pixelSize.x), 0);
@@ -157,10 +157,20 @@ public class RasterizationRenderer : MonoBehaviour {
                     var w2 = EdgeFunction (v0, v1, p);
                     if (w0 >= 0 && w1 >= 0 && w2 >= 0) {
                         // triangle contains the point
-                        var index = y * resolution.x + x;
-                        var position = vertexBuffer.positions[i] * (w0 / area) + vertexBuffer.positions[i + 1] * (w1 / area) + vertexBuffer.positions[i + 2] * (w2 / area);
-                        position /= position.w;
+                        w0 /= area;
+                        w1 /= area;
+                        w2 /= area;
 
+                        var z0 = 1f / vertexBuffer.positions[i].w;
+                        var z1 = 1f / vertexBuffer.positions[i + 1].w;
+                        var z2 = 1f / vertexBuffer.positions[i + 2].w;
+
+                        var position = new Vector3 ();
+                        position.x = v0.x * w0 + v1.x * w1 + v2.x * w2;
+                        position.y = v0.y * w0 + v1.y * w1 + v2.y * w2;
+                        position.z = z0 * w0 + z1 * w1 + z2 * w2;
+
+                        var index = y * resolution.x + x;
                         pixelBuffer.indexes.Add (index);
                         pixelBuffer.positions.Add (position);
                     }
@@ -174,7 +184,7 @@ public class RasterizationRenderer : MonoBehaviour {
         for (int i = 0; i < size; i++) {
             var index = pixelBuffer.indexes[i];
             var currentZ = pixelBuffer.positions[i].z;
-            if (currentZ <= depthBuffer[index]) {
+            if (currentZ > depthBuffer[index]) {
                 colorBuffer[index] = materialColor;
                 depthBuffer[index] = currentZ;
             }
